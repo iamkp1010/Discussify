e567ddeded7394e4b2142f225fe502a02137cbbd
Krupal Patel <krupalpatel150@gmail.com>
2024-02-07 11:32:37 +0530

    Fend deploy test

--- client/src/config.jsx
+++ client/src/config.jsx
@@ -1,2 +1,3 @@
 export const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID
-export const BACKEND_URL = "http://localhost:8000/api"
+// export const BACKEND_URL = "http://localhost:8000/api"
+export const BACKEND_URL = "https://discussify.onrender.com/api"
